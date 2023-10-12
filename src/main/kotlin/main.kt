92e6db5b19eed137d61caa1cc76860545bc245d4
Yaroslav Borisov <yarx@ya.ru>
2023-10-12 14:57:49 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    println(calcComission(payment = 80000))
+    println(calcComission("Мир", 10_000,1_000) )
+    println(calcComission("Maestro", 60_000,30_000) )
+}
+
+
+fun calcComission(cardType:String = "VK Pay", paymentsSum:Int = 0, payment:Int) : Int {
+    return when (cardType){
+        "VK Pay" -> 0
+        "Visa", "Мир" -> Math.max(35, (0.75 * payment / 100).toInt())
+        else -> if (paymentsSum + payment <= 75_000) 0 else ((paymentsSum + payment - 75_000) * .6 / 100 + 20).toInt()
+    }
+}
