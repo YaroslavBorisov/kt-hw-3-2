61314376e7cdb2faa7f32163b1d37be79206197a
Yaroslav Borisov <yarx@ya.ru>
2023-10-13 15:54:03 +0300

    Commit 2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,13 +6,29 @@ fun main() {
 >>>>>>> 24b2724 (Initial commit)
     println(calcComission("Мир", 10_000,1_000) )
     println(calcComission("Maestro", 60_000,30_000) )
+    println(calcComission("Maestro", 599_000,1_001) )
 }
 
 
 fun calcComission(cardType:String = "VK Pay", paymentsSum:Int = 0, payment:Int) : Int {
+    // Возвращаем -1, если исполнить платеж невозможно по причине превышения лимита
     return when (cardType){
-        "VK Pay" -> 0
-        "Visa", "Мир" -> Math.max(35, (0.75 * payment / 100).toInt())
-        else -> if (paymentsSum + payment <= 75_000) 0 else ((paymentsSum + payment - 75_000) * .6 / 100 + 20).toInt()
+        "VK Pay" -> if (isVKLimitReached(paymentsSum, payment))  -1 else 0
+        "Visa", "Мир" -> if (isCardLimitReached(paymentsSum, payment)) -1 else
+            Math.max(35, (0.75 * payment / 100).toInt())
+        else -> if (isCardLimitReached(paymentsSum, payment)) -1 else
+            if (paymentsSum + payment <= 75_000) 0 else ((paymentsSum + payment - 75_000) * .6 / 100 + 20).toInt()
     }
+}
+
+fun isVKLimitReached(paymentsSum: Int, payment: Int): Boolean {
+    return isPaymentsLimitReached(payment, paymentsSum)
+}
+
+private fun isPaymentsLimitReached(payment: Int, paymentsSum: Int, paymentLimit: Int = 15_000, monthlyPaymentsLimit: Int = 40_000): Boolean {
+    return payment > paymentLimit || paymentsSum + payment > monthlyPaymentsLimit;
+}
+
+fun isCardLimitReached(paymentsSum:Int = 0, payment:Int) : Boolean {
+    return isPaymentsLimitReached(payment, paymentsSum, 150_000, 600_000)
 }
