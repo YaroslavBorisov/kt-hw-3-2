f386c79dd3d0f13802a2d83d6cf4cf5a32974730
Yaroslav Borisov <yarx@ya.ru>
2023-10-13 16:00:40 +0300

    Added limit check

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,5 @@
 fun main() {
-<<<<<<< HEAD
-    println(calcComission(payment = 80000))
-=======
     println(calcComission(payment = 80_000))
->>>>>>> 24b2724 (Initial commit)
     println(calcComission("Мир", 10_000,1_000) )
     println(calcComission("Maestro", 60_000,30_000) )
     println(calcComission("Maestro", 599_000,1_001) )
